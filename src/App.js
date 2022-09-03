be4345cdf93cd926124cf1b328e01fc650c5d51b
DJ Drakos <dianajodrakos@gmail.com>
2022-09-03 15:06:19 -0700

    refactor component into module, add footer with attribution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
+import Component from './Component';
+import Footer from './Footer';
 
 function App() {
+  
   return (
     <div className="App">
-      <article className="card">
-        <img className="card-img" src="./image-qr-code.png" alt="https://frontendmentor.io"/>
-
-        <section className="card-text">
-          <h1 className="card-title">Improve your front-end skills by building projects
-          </h1>
-
-          <p className="card-p">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-          </p>
-        </section>
-      </article>
-
-      {/* <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-        Coded by <a href="https://github.com/dj-drakos/">DJ Drakos</a>.
-      </div> */}
+      <Component />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  
+  return (
+    <footer>
+      <section className="attribution">
+  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+  Coded by <a href="https://github.com/dj-drakos/">DJ Drakos</a>.
+      </section>
+    </footer>
+  );
+}
