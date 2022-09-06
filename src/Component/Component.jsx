f0cd131b0c779e372bf594d2aa8c51089465971f
DJ Drakos <dianajodrakos@gmail.com>
2022-09-06 10:52:52 -0700

    refactor to address accessibility issues

--- src/Component/Component.jsx
+++ src/Component/Component.jsx
@@ -1,13 +1,13 @@
 import './Component.css'
 
-export default function Component() {
+export default function Component({role}) {
 
   return (
-    <div className="Component">
+  <div className="Component" role={role}>
       <img src="./image-qr-code.png" alt="QR Code to go to Frontend Mentor"/>
       <section>
-        <h2>Improve your front-end skills by building projects
-        </h2>
+        <h1>Improve your front-end skills by building projects
+        </h1>
 
         <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
         </p>
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,7 +2,7 @@ import './Footer.css'
 export default function Footer() {
   
   return (
-    <footer className="attribution">
+    <footer role='contentinfo' className="attribution">
   Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
   Coded by <a href="https://github.com/dj-drakos/">DJ Drakos</a>.
     </footer>
